37e7965b43331eeee9da3cf3e82f26813d547309
zzahid93 <zohaibzahid1@gmail.com>
2019-12-10 11:22:29 +0000

    Refactored Randomizer

--- src/main/java/com/bae/bankingapp/Generator.java
+++ src/main/java/com/bae/bankingapp/Generator.java
@@ -4,27 +4,39 @@ import java.util.Random;
 
 public class Generator {
 	Random rand = new Random();
+
 	public String generatorLetter() {
-		switch(rand.nextInt(3)) {
-		case (0): return "A";
-		case (1): return "B";
-		case (2): return "C";
-	}
+		switch (rand.nextInt(3)) {
+		case (0):
+			return "A";
+		case (1):
+			return "B";
+		case (2):
+			return "C";
+		}
 		return "D";
 	}
+
 	public int generatorNumber() {
-		switch(rand.nextInt(3)) {
-		case (0): int a = 1000000000;
-				  return (a + rand.nextInt(899999999));
-		case (1): int b = 10000000;
-				  return (b + rand.nextInt(89999999));
-		case (2): int c = 100000;
-				  return (c + rand.nextInt(899999));
-		default:
-			int d = 190;
-			return d;
+
+		int account = 0;
+		switch (rand.nextInt(3)) {
+		case (0):
+			int a = 1000000000;
+			account = (a + rand.nextInt(899999999));
+			return account;
+		case (1):
+			int b = 10000000;
+			account = (b + rand.nextInt(89999999));
+			return account;
+		case (2):
+			int c = 100000;
+			account = (c + rand.nextInt(899999));
+			return account;
 		}
+		return account;
 	}
+
 	public static String accountNumber() {
 		Generator genLetter = new Generator();
 		Generator genNumber = new Generator();
